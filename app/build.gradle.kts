d37c043c43ac4df902e2fc94678b4b140f0840e6
Ossama Alali <ossman96@gmail.com>
2025-12-16 18:31:34 +0100

    chore(gradle): update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.material)
-    implementation(libs.androidx.material3.v140alpha07)
+    implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.extended)
 
     implementation(libs.androidx.glance.preview)
