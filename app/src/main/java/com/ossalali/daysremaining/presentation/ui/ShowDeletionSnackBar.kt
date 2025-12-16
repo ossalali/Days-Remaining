78a3f58fc7a94d279209dea601381207e7911771
Ossama Alali <ossman96@gmail.com>
2025-12-16 18:57:19 +0100

    chore(UI): replace Icons Library with drawables for ShowDeletionSnackBar.kt

--- app/src/main/java/com/ossalali/daysremaining/presentation/ui/ShowDeletionSnackBar.kt
+++ app/src/main/java/com/ossalali/daysremaining/presentation/ui/ShowDeletionSnackBar.kt
@@ -1,8 +1,6 @@
 package com.ossalali.daysremaining.presentation.ui
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Snackbar
@@ -11,7 +9,9 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import com.ossalali.daysremaining.MyAppTheme
+import com.ossalali.daysremaining.R
 import com.ossalali.daysremaining.presentation.ui.previews.DefaultPreviewsNoSystemUI
 import com.ossalali.daysremaining.presentation.ui.theme.Dimensions
 
@@ -22,17 +22,20 @@ fun ShowDeletionSnackBar(
     snackBarMessage: String,
 ) {
     Snackbar(
-      modifier = Modifier.padding(all = Dimensions.default),
-      action = {
-          TextButton(onClick = onUndoDelete) {
-              Text("Undo", color = Color.hsl(267.44f, 0.9707f, 0.598f))
-          }
-      },
-      dismissAction = {
-          IconButton(onClick = onDismissSnackBar) {
-              Icon(imageVector = Icons.Filled.Close, contentDescription = "Close snackbar")
-          }
-      },
+        modifier = Modifier.padding(all = Dimensions.default),
+        action = {
+            TextButton(onClick = onUndoDelete) {
+                Text("Undo", color = Color.hsl(267.44f, 0.9707f, 0.598f))
+            }
+        },
+        dismissAction = {
+            IconButton(onClick = onDismissSnackBar) {
+                Icon(
+                    painter = painterResource(R.drawable.close_24px),
+                    contentDescription = "Close snackbar"
+                )
+            }
+        },
     ) {
         Text(text = snackBarMessage)
     }
